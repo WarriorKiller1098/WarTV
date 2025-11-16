419470320b774ec82fbc17a80af7ccdec6cb78a6
WarriorKiller1098 <gotimenowinc123@gmail.com>
2025-11-16 16:10:06 +0530

    The start of wartv
    The start, the wonders are starting now of WarTV, a dumb TV network.
    
    It's gonna be on the internet
    
    It's gonna be very cool
    
    It's gonna be available for anyone
    
    Yayayyayayay

---
+++ app.js
@@ -0,0 +1,24 @@
+const url = require("url") 
+const http = require("http") 
+const port = 10000
+const fs = require("fs") 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+http.createServer(function(req, res) {
+    const urlpath = url.parse(req.url, true) 
+    const parsedpath = urlpath.path
+    let query = urlpath.query
+    const path = parsedpath.split("?")[0]
+    if (path.charAt(path.length - 1) == "?") {
+        query = parsedpath.split("?")[1].replace("?", "")
+    }
+    console.log("Method requested: " + req.method + ", with endpoint " + path);
+      if (path == "/") {
+        sendf(res, "index.html");
+      }
+}}).listen(port);
+
+function sendf(res, file) {
+    res.write(fs.readFileSync(__dirname + "/" + file));
+    res.end();
+}
